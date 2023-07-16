4296689c4be90e3a3d2157e421eebaa00c2fb3bd
Felipe Rosa Gomes <redesigninformatica@gmail.com>
2023-07-16 17:20:57 -0300

    Criando tela com formulário

--- lib/src/config/app_data.dart
+++ lib/src/config/app_data.dart
@@ -1,5 +1,6 @@
 import 'package:greengrocer/src/models/cart_item_model.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/models/user_model.dart';
 
 ItemModel apple = ItemModel(
   description:
@@ -77,12 +78,20 @@ List<CartItemModel> cartItems = [
     item: apple,
     quantity: 1,
   ),
-   CartItemModel(
+  CartItemModel(
     item: mango,
     quantity: 1,
   ),
-   CartItemModel(
+  CartItemModel(
     item: guava,
     quantity: 3,
   ),
 ];
+
+UserModel user = UserModel(
+  celular: '99 9 9999-9999',
+  cpf: "999.999.999-99",
+  email: "redesigninformatica@gmail.com",
+  name: 'Felipe Gomes',
+  senha: '',
+);
---
+++ lib/src/models/user_model.dart
@@ -0,0 +1,16 @@
+class UserModel{
+  String name;
+  String email;
+  String celular;
+  String cpf;
+  String senha;
+
+  UserModel({
+    required this.celular,
+    required this.cpf,
+    required this.email,
+    required this.name,
+    required this.senha,
+  });
+}
+
